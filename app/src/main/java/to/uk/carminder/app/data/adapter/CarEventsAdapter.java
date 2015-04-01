fc8260702d152b810de5bbef7a4401bc27e0ff76
Ovidiu Maja <ovi_dan89@yahoo.com>
2015-04-01 21:37:15 +0300

    Modifications for car events UI.

--- app/src/main/java/to/uk/carminder/app/data/adapter/CarEventsAdapter.java
+++ app/src/main/java/to/uk/carminder/app/data/adapter/CarEventsAdapter.java
@@ -1,7 +1,24 @@
 package to.uk.carminder.app.data.adapter;
 
-/**
- * Created by ovidiu on 3/21/15.
- */
-public class CarEventsAdapter {
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+public class CarEventsAdapter extends CursorAdapter{
+
+    public CarEventsAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
 }
